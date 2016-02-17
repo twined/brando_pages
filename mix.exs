840eaec3d28a7063bddf1285610f89d0264758fc
Trond Mjøen <trond@twined.net>
2016-02-17 12:43:05 +0100

    Update brando & use new registry

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule BrandoPages.Mixfile do
      {:ex_machina, "~> 0.6.1", only: :test},
      {:excoveralls, "~> 0.4", only: :test},
 
-     {:brando, github: "twined/brando"},
+     {:brando, github: "twined/brando", optional: true},
 
      # Documentation dependencies
      {:ex_doc, "~> 0.11", only: :docs},
