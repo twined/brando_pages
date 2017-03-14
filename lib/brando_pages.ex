88867ec1de5858ad2bab4d581d4c6c4760de5b81
Trond Mjøen <trond@twined.net>
2017-03-14 11:02:43 +0100

    Add context

--- lib/brando_pages.ex
+++ lib/brando_pages.ex
@@ -1,2 +1,15 @@
-defmodule BrandoPages do
+defmodule Brando.Pages do
+  @moduledoc """
+  Context for Brando.Pages
+  """
+  alias Brando.Pages.Page
+
+  def get_page(key) do
+    page = Brando.repo.get_by(Page, key: key)
+
+    case page do
+      nil  -> {:error, {:page, :not_found}}
+      page -> {:ok, page}
+    end
+  end
 end
